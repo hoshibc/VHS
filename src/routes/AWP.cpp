48f5ed43d0d7741eebf54fe78e5d200cbf7a81e8
unknown <brianhan65@hotmail.com>
2024-07-14 10:15:24 -0700

    autos

--- src/routes/AWP.cpp
+++ src/routes/AWP.cpp
@@ -8,10 +8,41 @@
 
 void AWP(){
     PIDDataSet TestPara={1.5,0.1,0.15}; //initialize
-    Pistake.set(false);
-    RunRoller(-100);
-    wait(300, msec);
-    MoveEncoderPID(TestPara, 100, 5, 0.3, 0, true);
+    Pistake.set(false); //intake down
+    Tilt.set(false); // ohly tilt 
+    RunRoller(-100);//
+    wait(200, msec);
+    MoveEncoderPID(TestPara, 100, 2.2, 0.3, 0, true);
     RunRoller(0);
+    MoveEncoderPID(TestPara, -100, 5, 0.3, 0, true ); // drive forward to clear ring
+    wait(200, msec);
+    TurnMaxTimePID(TestPara, 100, 0.5, true );        
+    MoveEncoderPID(TestPara, 100, 6, 0.3, 100, true);
+    TurnMaxTimePID(TestPara, 50, 0.3, true);
+    MoveEncoderPID(TestPara, 100, 3, 0.3, 57, true);
+    wait(100,msec);
+    RunRoller(-100); //score onto alliance stake
+    wait(1000, msec);
+    MoveEncoderPID(TestPara, -100, 3, 0.3, 57, true);
+    TurnMaxTimePID(TestPara, -87, 0.5, true);
+    Tilt.set(true); // down to grab mogo
+
+    MoveEncoderPID(TestPara, 100, 25, 0.3, -87, false);
+    MoveEncoderPID(TestPara, 40, 25, 0.3, -87, true); //slow down to grab mogo
+    Clamp.set(true); //close clamp 
+    wait(200,msec);
+    MoveEncoderPID(TestPara, 100, 0.2, 0.4,-87, true); //move back to let goal sit in
+    Tilt.set(false); // tilt mogo
+    RunRoller(-100); //run intake
+    TurnMaxTimePID(TestPara, 107, 0.8,true);
+    MoveEncoderPID(TestPara, -100, 23, 0.4, 107, true); //move back to let goal sit in
+    TurnMaxTimePID(TestPara, 140,0.4,true);
+    MoveEncoderPID(TestPara, -60, 10, 0.4, 140, true); //grab ring 3
+    TurnMaxTimePID(TestPara, -100,0.4,true); 
+    MoveEncoderPID(TestPara, -100, 10, 0.4, -100, true); //move back to let goal sit in
+
+
+
+
 
 }
