d9ccaa7615129d6df3b5797b89911cddf6da629f
unknown <brianhan65@hotmail.com>
2024-07-15 11:29:48 -0700

    booty

--- src/routes/AWP.cpp
+++ src/routes/AWP.cpp
@@ -24,18 +24,18 @@ void AWP(){
     RunRoller(-100); //score onto alliance stake
     wait(1000, msec);
     MoveEncoderPID(TestPara, -100, 3, 0.3, 57, true);
-    TurnMaxTimePID(TestPara, -87, 0.5, true);
+    TurnMaxTimePID(TestPara, -85, 0.5, true);
     Tilt.set(true); // down to grab mogo
 
     MoveEncoderPID(TestPara, 100, 25, 0.3, -87, false);
     MoveEncoderPID(TestPara, 40, 25, 0.3, -87, true); //slow down to grab mogo
     Clamp.set(true); //close clamp 
     wait(200,msec);
-    MoveEncoderPID(TestPara, 100, 0.2, 0.4,-87, true); //move back to let goal sit in
+    MoveEncoderPID(TestPara, 100, 2, 0.4,-87, true); //move back to let goal sit in
     Tilt.set(false); // tilt mogo
     RunRoller(-100); //run intake
     TurnMaxTimePID(TestPara, 107, 0.8,true);
-    MoveEncoderPID(TestPara, -100, 23, 0.4, 107, true); //move back to let goal sit in
+    MoveEncoderPID(TestPara, -100, 20, 0.4, 107, true); //move back to let goal sit in
     TurnMaxTimePID(TestPara, 140,0.4,true);
     MoveEncoderPID(TestPara, -60, 10, 0.4, 140, true); //grab ring 3
     TurnMaxTimePID(TestPara, -100,0.4,true); 
